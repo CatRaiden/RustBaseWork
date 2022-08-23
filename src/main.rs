d83b171d048baae4e5b4ff82d49994fad58050cd
KevinSie <kevinsie223@gmail.com>
2022-08-23 16:09:13 +0800

    add func

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 fn main() {
     println!("Hello, CatRaiden world!");
+
+    //if
+    let number = 3;
+    if number < 5 {
+        println!("true");
+    } else {
+        println!("false");
+    }
+
+    //other function
+    other_function();
+
+    //while
     let mut i = 0;
     while i < 10 {
         i+=1; 
         println!("i number {}", i);       
     }
+
+
+}
+
+fn other_function() {
+    println!("here is other function");
 }
